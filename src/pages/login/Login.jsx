d6d8d3ce2f1d4b0ab60c4464f34299a9f45cce76
Gimasha Ranasinghe <gimashakranasinghe@gmail.com>
2024-12-25 04:01:14 +1100

    Login page added

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,29 @@
 import "./login.scss";
 
 const Login = () => {
-  return <div className="login">Login</div>;
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Welcome to Mickey!</h1>
+          <p>
+            Where memes are born, friends are made, and boredom goes to die.
+            Sign up now and let's make your feed fun!
+          </p>
+          <span>Don't you have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
