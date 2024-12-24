2626b791daa05204120897a7385035f1795107c3
Gimasha Ranasinghe <gimashakranasinghe@gmail.com>
2024-12-25 06:41:40 +1100

    Register page added

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,36 @@
 import "./register.scss";
 
 const Register = () => {
-  return <div className="register">Register</div>;
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>
+            Hello!
+            <br />
+            Welcome to Mickey...
+          </h1>
+          <p>
+            Where memes are born, friends are made, and boredom goes to die.
+            Sign up now and let's make your feed fun!
+          </p>
+          <span>Do you have an account?</span>
+          <button>Login</button>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="First Name" />
+            <input type="text" placeholder="Last Name" />
+            <input type="email" placeholder="Email" />
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
