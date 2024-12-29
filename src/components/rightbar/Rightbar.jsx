a2a5c2c484047c80a570bd3a77fb8459c35dcc74
Gimasha Ranasinghe <gimashakranasinghe@gmail.com>
2024-12-30 04:08:36 +1100

    right bar added

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,7 +1,148 @@
 import "./rightbar.scss";
 
 const Rightbar = () => {
-  return <div className="rightbar">Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="container">
+        <div className="item">
+          <span>Suggested for you</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/sunflower-and-red-butterfly-garry-gay.jpg"
+                alt=""
+              />
+              <span>Nipuni Ranasinghe</span>
+            </div>
+            <div className="buttons">
+              <button>Add</button>
+              <button>Delete</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/sunflower-and-red-butterfly-garry-gay.jpg"
+                alt=""
+              />
+              <span>Nipuni Ranasinghe</span>
+            </div>
+            <div className="buttons">
+              <button>Add</button>
+              <button>Delete</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/sunflower-and-red-butterfly-garry-gay.jpg"
+                alt=""
+              />
+              <span>Nipuni Ranasinghe</span>
+            </div>
+            <div className="buttons">
+              <button>Add</button>
+              <button>Delete</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/sunflower-and-red-butterfly-garry-gay.jpg"
+                alt=""
+              />
+              <p>
+                <span>Gimasha Ranasinghe</span> changed their display picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/sunflower-and-red-butterfly-garry-gay.jpg"
+                alt=""
+              />
+              <p>
+                <span>Gimasha Ranasinghe</span> changed their display picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/sunflower-and-red-butterfly-garry-gay.jpg"
+                alt=""
+              />
+              <p>
+                <span>Gimasha Ranasinghe</span> changed their display picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="item">
+          <span>My Friends</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/sunflower-and-red-butterfly-garry-gay.jpg"
+                alt=""
+              />
+              <div className="online" />
+              <span>Gimasha Ranasinghe</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/sunflower-and-red-butterfly-garry-gay.jpg"
+                alt=""
+              />
+              <div className="online" />
+              <span>Gimasha Ranasinghe</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/sunflower-and-red-butterfly-garry-gay.jpg"
+                alt=""
+              />
+              <div className="online" />
+              <span>Gimasha Ranasinghe</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/sunflower-and-red-butterfly-garry-gay.jpg"
+                alt=""
+              />
+              <div className="online" />
+              <span>Gimasha Ranasinghe</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/sunflower-and-red-butterfly-garry-gay.jpg"
+                alt=""
+              />
+              <div className="online" />
+              <span>Gimasha Ranasinghe</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Rightbar;
